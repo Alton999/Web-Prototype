8f0ecee58f19ea941669bb590279aba2e4701f66
Alton999 <alton_ong@live.com.sg>
2020-11-25 20:53:45 +1100

    Added community page

---
+++ community/community.js
@@ -0,0 +1,10 @@
+//Navbar initialisation
+const navbar = document.getElementById("myTopNav");
+const openNavButton = document.getElementById("open-nav");
+openNavButton.onclick = function () {
+	if (navbar.className === "nav") {
+		navbar.className += " responsive";
+	} else {
+		navbar.className = "nav";
+	}
+};
